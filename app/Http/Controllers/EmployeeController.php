94a03093555559379921b3a6249c52fbc5b3d82e
dharmik <dharmikd@zignuts.com>
2023-04-04 10:50:46 +0530

    perform crud operation on Employee Module

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,27 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
     public function create(Request $request){
-        return success('Create Access');
+        $request->validate([
+            'name'          => 'required|string|max:30',
+            'email'         => 'required|unique:employees,email',
+            'designation'   => 'required|string',
+            'salary'        => 'required|numeric'
+        ]);
+        $employee = Employee::create($request->only('name','email','designation','salary'));
+        return success('Employee Created Successfully',$employee);
     }
 
     public function list(){
-        return success('List Access');
+        $employee = Employee::all();
+        if($employee){
+            return success('Employee List',$employee);
+        }
+        return error('Data Not Found',type:'notfound');
     }
 
-    public function update(Request $request,$id){
-        return success('Update Access');
+    public function update(Request $request,Employee $id){
+        $request->validate([
+            'name'          => 'string|max:30',
+            'email'         => 'email',
+            'designation'   => 'string',
+            'salary'        => 'numeric'
+        ]);
+        if($id){
+            $id->update($request->only('name','email','designation','salary'));
+            return success('Your Employee Is Updated SuccessFully',$id);
+        }
+        return error('Your Employee Is Not Updated',type:'notfound');
     }
 
     public function delete($id){
-        return success('Delete Access');
+        $employee = Employee::find($id);
+        if($employee){
+            $employee->delete();
+            return success('Employee Deleted Successfully');
+        }
+        return error('Employee Not Deleted');
     }
 
     public function get($id){
-        return success('Get Data By Id');
+        $employee = Employee::findOrFail($id);
+        return success('Get Employee Data By ID',$employee);
     }
 }
